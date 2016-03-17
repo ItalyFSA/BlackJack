8cc907cb714febe4b00e0db053f10bd3e061cf11
kmlamkin <katie@alamkin.com>
2016-03-17 16:53:39 -0700

    Added deck constructor and reset function

--- Deck.java
+++ Deck.java
@@ -6,10 +6,16 @@ class Deck
 		this.deck = new Card[52];
 		for (int i = 1; i < 14; i++) {
 			// must account for face cards only having value of 10
-			this.deck[i - 1] = new Card(i, Suit.Spades);
-			this.deck[i + 12] = new Card(i, Suit.Diamonds);
-			this.deck[i + 25] = new Card(i, Suit.Clubs);
-			this.deck[i + 38] = new Card(i, Suit.Hearts);
+			int curValue = i;
+			if (i >= 10)
+				curValue = 10;
+			if (i == 1)
+				curValue = 11;
+			// must account for face cards only having value of 10
+			this.deck[i - 1] = new Card(curValue, Suit.Spades);
+			this.deck[i + 12] = new Card(curValue, Suit.Diamonds);
+			this.deck[i + 25] = new Card(curValue, Suit.Clubs);
+			this.deck[i + 38] = new Card(curValue, Suit.Hearts);
 		}
 	}
 
@@ -30,6 +36,19 @@ class Deck
 
 	public void reset()
 	{
-
+		this.deck = new Card[52];
+		for (int i = 1; i < 14; i++) {
+			// must account for face cards only having value of 10
+			int curValue = i;
+			if (i >= 10)
+				curValue = 10;
+			if (i == 1)
+				curValue = 11;
+			// must account for face cards only having value of 10
+			this.deck[i - 1] = new Card(curValue, Suit.Spades);
+			this.deck[i + 12] = new Card(curValue, Suit.Diamonds);
+			this.deck[i + 25] = new Card(curValue, Suit.Clubs);
+			this.deck[i + 38] = new Card(curValue, Suit.Hearts);
+		}
 	}
 }
