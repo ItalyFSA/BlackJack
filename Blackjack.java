cd26831cee281f9a9f76897819d480389dfe2140
kmlamkin <katie@alamkin.com>
2016-03-31 10:42:37 -0700

    update blackjack functions

--- Blackjack.java
+++ Blackjack.java
@@ -1,3 +1,5 @@
+import java.io.*;
+
 class Blackjack
 {
 	public static void main(String[] args)
@@ -8,11 +10,97 @@ class Blackjack
 			runDeckTests();
 			runDealerTests();
 			runPlayerTests();
+
+			Player player = new Player();
+			Dealer dealer = new Dealer();
+
+			Deck deck = new Deck();
+
+			runGame(player, dealer, deck);
 		}
 		catch (Exception e)
 		{
 			System.out.println("Error: " + e.getMessage());
 		}
+	}
+
+	public static void runGame(Player player, Dealer dealer, Deck deck)
+	{
+		newHand(player, dealer, deck);
+
+		if (player.blackjack())
+		{
+			// player wins
+			whoWon(player, dealer);
+		}
+		else if (dealer.blackjack())
+		{
+			// dealer wins
+			whoWon(player, dealer);
+		}
+		else
+		{
+			boolean run = true;
+			while (run)
+			{
+				try	
+				{
+					System.out.println("hit or stand? (please type as written)");
+					String input = (new BufferedReader(new InputStreamReader(System.in))).readLine();
+					if (input == "hit")
+					{
+						boolean end = hit(player, deck);
+						if (end)
+							run = false;
+					}
+					else if (input == "stand")
+					{
+						stand();
+						run = false;
+					}
+					else
+					{
+						System.out.println("Didn't understand input please try again.");
+					}
+				}
+				catch (Exception e)
+				{
+					System.out.println("Error with taking input. Abort.");
+					run = false;
+				}
+				
+			}
+		}
+	}
+
+	public static void newHand(Player player, Dealer dealer, Deck deck)
+	{
+		
+	}
+
+	public void dealNewHand()
+	{
+		
+	}
+
+	public static boolean hit(Player player, Deck deck)
+	{
+		return false;
+	}
+
+	public static void stand()
+	{
+		
+	}
+
+	public void playDealerHand(Dealer dealer, Deck deck)
+	{
+		
+	}
+
+	public void static whoWon(Player player, Dealer dealer)
+	{
+		
 	}
 
 	public static void runDeckTests()
